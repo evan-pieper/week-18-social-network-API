1aed9f490d67a661dcb22e5889a8884365a9d508
evan-pieper <evanjpieper@gmail.com>
2023-11-06 21:20:09 -0800

    added logic for thought schema

---
+++ models/Thought.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+import reactionSchema from './Reaction.js';
+
+const thoughtSchema = new Schema({
+  thoughtText: {type: String, required: true, minlength: 1, maxlength: 280 },
+  createdAt: {type: Date, default: Date.now, get: (timestamp) => dateFormat(timestamp)},
+  username: {type: String, required: true},
+  reactions: [reactionSchema] // use ReactionSchema to validate data for a reaction
+}, {
+  toJSON: {
+    getters: true, // tells the schema to use getter functions specified in the schema when data is requested
+    virtuals: true // tells the schema to include virtuals when json data is sent
+  },
+  id: false
+});
+
+thoughtSchema.virtual('reactionCount').get(function() { // virtual that returns the number of reactions in a thought's reactions array
+  return this.reactions.length;
+});
+
+
+const Thought = mongoose.model('Thought', thoughtSchema);
+module.exports = Thought;
