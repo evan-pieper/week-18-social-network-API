4f54ca40e3f1d4641e31e0983541cb3688c370a3
evan-pieper <evanjpieper@gmail.com>
2023-11-06 21:33:18 -0800

    added Reaction model logic, and basic file structure for controllers

---
+++ models/Reaction.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+// Schema for reactions (subdocument of Thought model)
+const reactionSchema = new Schema({
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new mongoose.Types.ObjectId() // Generates a new unique identifier for each reaction
+    },
+    reactionBody: {type: String, required: true, maxlength: 280},
+    username: {type: String, required: true},
+    createdAt: {type: Date, default: Date.now, get: (timestamp) => dateFormat(timestamp)}
+}, {
+    toJSON: {
+        getters: true // Applies the getter function to the data when it's requested
+    }
+});
+
+// Since this isn't a model but a subdocument schema for the Thought model, we don't compile it with mongoose.model(). Instead, we export the schema directly.
+module.exports = reactionSchema;
