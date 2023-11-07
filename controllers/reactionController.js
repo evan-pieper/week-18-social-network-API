d4be8a889df04c4d821d210aa14ad54479b6cbc5
evan-pieper <evanjpieper@gmail.com>
2023-11-07 05:30:25 -0800

    added comments to reaction controller

--- controllers/reactionController.js
+++ controllers/reactionController.js
@@ -4,13 +4,13 @@ const Thought = require('../models/Thought');
 module.exports = {
     async createReaction(req, res) {
         try {
-            // Find the thought by ID and update it by pushing a new reaction into the reactions array
-            const thought = await Thought.findByIdAndUpdate(
+            const thought = await Thought.findByIdAndUpdate( // Find thought by ID and update it by pushing a reaction to the reactions array
               req.params.thoughtId,
               { $push: { reactions: req.body } },
               { new: true, runValidators: true } // Return the updated thought
             );
             if (!thought) {return res.status(404).json({ message: 'No thought found with this id!' });} // If no thought with that ID, return 404
+
             res.status(200).json(thought); // Otherwise, return updated thought
           } catch (error) {
             res.status(500).json({ message: 'Error adding reaction', error: error });
@@ -18,18 +18,14 @@ module.exports = {
     },
     async deleteReaction(req, res) {
         try {
-            // Find the thought by ID and update it by pulling a reaction from the reactions array
-            const thought = await Thought.findByIdAndUpdate(
+            const thought = await Thought.findByIdAndUpdate( // Find thought by ID and update it by pulling a reaction from the reactions array
               req.params.thoughtId,
               { $pull: { reactions: { _id: req.params.reactionId } } },
               { new: true }
-            );
-        
-            if (!thought) {
-              return res.status(404).json({ message: 'No thought found with this id!' });
-            }
-        
-            res.status(200).json(thought);
+            );    
+            if (!thought) {return res.status(404).json({ message: 'No thought found with this id!' });} // If no thought with that ID, return 404
+    
+            res.status(200).json(thought); // Otherwise, return updated thought (without the deleted reaction)
           } catch (error) {
             res.status(500).json({ message: 'Error removing reaction', error: error });
           }
